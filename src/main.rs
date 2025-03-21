3615738027bc584e934177a07ccd828e78cf4b00
xxlovett0706 <xxlovett0706@gmail.com>
2025-03-21 18:38:30 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, rust!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_main() {
+        main();
+    }
 }
